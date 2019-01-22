020f034d443e8ebac7e97ee6f6e84e93ed09a595
Hina <hina_r@hotmail.fr>
2019-01-22 18:56:24 +0100

    mise à jour admin Billet

---
+++ adminBillet.php
@@ -0,0 +1,31 @@
+<?php
+require_once('libs/global.php');
+
+// checkAccess();
+
+$actualCarnet = $_GET['goto'];
+
+
+require_once('page/header.php');
+$listBillet = find ('SELECT * FROM billet');
+?>
+<div class="container">
+
+	<?php
+		echo '<h1>Ma liste de carnet</h1><hr>';
+
+		foreach($listBillet as $billet)
+		{
+			echo '<div class="alert alert-primary" role="alert"><b>' . $billet['Titre'] . '</b> ' . $billet['Artiste'] . '</b> ' .$billet['Description'] . '<a href="adminBillet.php?suppr=' . $billet['id'] . '" class="btn btn-danger">X</a></div>';
+
+		}
+	?>
+
+<?php
+echo '<a href="editBillet.php?goto=' . $actualCarnet . '" class="btn btn-danger">Ajouter un billet</a>'
+?>
+
+</div>
+<?php
+require_once('page/footer.php');
+?>
