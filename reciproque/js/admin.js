459de91a733372cd33b5002c33d0bedc94ff1a22
theoverdier <45787432+theoverdier@users.noreply.github.com>
2019-02-22 09:53:36 +0100

    Problème des slides réglé

--- reciproque/js/admin.js
+++ reciproque/js/admin.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 		$('.modal form input[type=hidden]').val($(this).data('billet'));
 
 		$('.carousel-item:not(:first-child)').remove();
+		$('.carousel-item').addClass('active');
+
 		$(this).find('.listPhotos img').each(function() {
 			$('.carousel-inner').append('<div class="carousel-item">' + $(this).prop('outerHTML') + '</div>');
 		});
